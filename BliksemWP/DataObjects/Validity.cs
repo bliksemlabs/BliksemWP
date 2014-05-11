db773773693d5b724be5e0cc65fd7fe0d8504788
Joel Haasnoot <joelhaasnoot@gmail.com>
2014-05-11 13:07:11 +0200

    Add some missing files

---
+++ BliksemWP/DataObjects/Validity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BliksemWP.DataObjects
+{
+    [DataContract]
+    class Validity
+    {
+        [DataMember(Name="from")]
+        public string From { get; set; }
+        public DateTime DateFrom
+        {
+            get
+            {
+                return DateTime.Parse(From);
+            }
+        }
+        [DataMember(Name = "to")]
+        public string To { get; set; }
+        public DateTime DateTo
+        {
+            get
+            {
+                return DateTime.Parse(To);
+            }
+        }
+    }
+}
